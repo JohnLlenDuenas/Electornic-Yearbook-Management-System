cbba1abf4854e497da28ed6594cfd3c448f2c2a7
JohnLlenDuenas <152172679+JohnLlenDuenas@users.noreply.github.com>
2024-11-09 21:41:25 +0800

    up

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(session({
   secret: '3f8d9a7b6c2e1d4f5a8b9c7d6e2f1a3b',
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/EYBMS_DB' }),
+  store: MongoStore.create({ mongoUrl: 'mongodb+srv://johnllentv:johnllentv@cluster0.pgaelxg.mongodb.net/EYBMS_DB?retryWrites=true&w=majority&appName=Cluster0' }),
   rolling: true,
   cookie: {
     maxAge: 15 * 60 * 1000,
